614b216cf3ebbea9571dfa350d56a52fd0a6c88a
nelsonpetro <nelsonpetrodoria@gmail.com>
2025-09-02 14:54:19 -0500

    First week activities - cse212

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -6,6 +6,7 @@ public static class Arrays
     /// integer greater than 0.
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
+    
     public static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
@@ -13,7 +14,16 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        //Do not add check for positivity since we assumed that above
+        //number and length are positive
+        //We can return an expression so we avoid declaring new variables
+        //Enumerable.Range(1, length) will provide a sequence of integers we can use
+        //This sequence of numbers will start at one at will stop at length
+        //With select we will map each value to a new value
+        //x => x * number is a lambda function to multiply the number by each number in the sequence
+        //ToArray() convert the sequence we create witj Enumerable into an array of doubles as requested.
+        return Enumerable.Range(1, length).Select(x => x * number).ToArray();
+
     }
 
     /// <summary>
@@ -23,11 +33,29 @@ public static class Arrays
     ///
     /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
     /// </summary>
+
     public static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        //Check if the amount is 0
+        //Declare a variable to hold the amount
+        int n = amount;
+        //If amount is 0 or the list count, then we do not have to rotate any element in the list
+        if (n == 0 || n == data.Count)
+        {
+            return;
+        }
+        //Get the new list holding the part of the list we want to rotate
+        //Use the GetRange(index, count) method
+        List<int> rotated = data.GetRange(data.Count - n, n);
+        //Add it to the beginning of the list using the InsertRange(index, list or array) method
+        data.InsertRange(0,rotated);
+        //Remove it from the end of the list using the RemoveRange(index, count) method
+        data.RemoveRange(data.Count - n, n);
+        //we do not return anything here because is a void method.
     }
 }
